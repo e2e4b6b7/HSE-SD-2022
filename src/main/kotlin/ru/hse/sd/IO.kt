de1a2526818cba0a6b99399167cd3d64d734b0db
Roman Venediktov <e2e4b6b7@gmail.com>
2022-02-01 08:29:52 +0300

    Style fix

--- src/main/kotlin/ru/hse/sd/IO.kt
+++ src/main/kotlin/ru/hse/sd/IO.kt
@@ -1,6 +1,6 @@
 package ru.hse.sd;
 
-import java.io.*
+import java.io.InputStream
+import java.io.OutputStream
 
 data class IO(val inputStream: InputStream, val outputStream: OutputStream, val errorStream: OutputStream)
-
