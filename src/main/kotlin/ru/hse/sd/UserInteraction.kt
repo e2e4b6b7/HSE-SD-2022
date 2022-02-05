9730729809240bd1bf3b9bdb70c1761d57848689
DF5HSE <fild10@yandex.ru>
2022-02-05 11:51:02 +0300

    Add UserInteraction

---
+++ src/main/kotlin/ru/hse/sd/UserInteraction.kt
@@ -0,0 +1,16 @@
+package ru.hse.sd
+
+import java.io.InputStream
+import java.io.OutputStream
+
+class UserInteraction(private val inputStream: InputStream = System.`in`,
+                      private val outputStream: OutputStream = System.out) {
+    companion object {
+        private const val COMMAND_LINE_PREFIX = ">>> "
+    }
+
+    fun read(): String {
+        outputStream.write(Companion.COMMAND_LINE_PREFIX)
+        return inputStream.readBytes().toString()
+    }
+}
